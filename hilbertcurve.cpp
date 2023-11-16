932bd4018fc7b1454b25b926ebba1641982d361d
aperalta8 <144752611+aperalta8@users.noreply.github.com>
2023-11-15 19:36:09 -0500

    Update hilbertcurve.cpp

--- hilbertcurve.cpp
+++ hilbertcurve.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <string>
 
+void drawHilbertCurve(int level, std::string& commands) {
+
+  // implement recursive function
+
+}
+
+void outputCurve(int level, const std::string& filename) { 
+
+  // 
+
+
+int main(int argc, char* argv[]) {
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " <level> <output_filename>\n";
+    return 1;
+  }
+
+  int level = std::stoi(argv[1]);
+  std::string outputFilename = argv[2];
+
+  outputCurve(level, outputFilename)
+
+  return 0;
+}
