d0a173e7ea43b070bbcd5c4b3d70dee87c79283c
aperalta8 <144752611+aperalta8@users.noreply.github.com>
2023-11-27 23:33:23 -0500

    Update hilbertcurve.cpp

--- hilbertcurve.cpp
+++ hilbertcurve.cpp
@@ -42,24 +42,21 @@ void appendRightCurve(std::stringstream& ss, int level) {
 
 // handles cla and writes the curve data to a file
 int main(int argc, char* argv[]) {
-  if (argc != 3) {
-    std::cerr << "Usage: " << argv[0] << " <level> <output_filename>\n";
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <level>\n";
     return 1;
   }
 
   int level = std::stoi(argv[1]); // first argument is level of curve
-  std::string outputFilename = argv[2]; // second argument is output file name
+  std::ofstream of("l-system.txt"); // opens file stream
 
-  std::string hilberCurve = drawHilbertCurve(level); // generates the hilbert curve
-  
-  std::ofstream outFile(outputFilename); // file opens for writing
-  if (!outFile.is_open()) {
-    std::cerr << "Error: Unable to open file '" << outputFilename << "' for writing.\n";
-    return 1;
+  if(!of.is_open()) { 
+    std::cerr << "Error: Unable to open file 'l-system.txt' for writing.\n";
   }
 
-  outFile << hilbertCurve;
-  outFile.close();
+  std::string hilbertCurve = drawHilbertCurve(level); // generates the hilbert curve
+  of << hilbertCurve; // writes curve to file
+  of.close();
   std::cout << "Hilbert curve commands written to '" << outputFilename << "'\n";
   
   return 0;
