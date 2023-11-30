6aa3a5f334198d8444f06839b5b7e7def8bc70f1
JZURI1 <123573769+JZURI1@users.noreply.github.com>
2023-11-30 15:03:51 -0500

    Update kochsnowflake.cpp

--- kochsnowflake.cpp
+++ kochsnowflake.cpp
@@ -1,51 +1,52 @@
-//Starter code used from Lab 6
+// Header inclusions
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <cmath>
 
-std::string koch_snowflake(int degree);
-std::string snowflake(int degree);
+// Function declarations
+std::string Koch_snowflake(int degree);
+std::string Snowflake(int degree);
 
 int main(){
+
+    // Ofstream to output the restlt of the koch function
     std::ofstream of("l-system.txt");
 
     int degree = 3;
 
-    of << koch_snowflake(degree);
+    of << Koch_snowflake(degree);
 }
 
-// *** DO NOT TOUCH! ***
-std::string koch_snowflake(int degree){
+// Generates 4 'sides' of the snowflake
+std::string Koch_snowflake(int degree){
     std::string commands = "";
 
-	// This handles generating the 4 'sides' of the snowflake.
     for(int i = 0; i < 4; i++){
-        commands += snowflake(degree);
+        commands += Snowflake(degree);
         commands += "+ + ";
     }
 
     return commands;
 }
 
-std::string snowflake(int degree){
+// Recursive function for generating the snowflake
+std::string Snowflake(int degree){
     if(degree == 0){
         return "F ";
     }
     std::string commands = "";
 
-	 // Logic for generating the snowflake goes here.
-
+    // Logic for generating the snowflake
     // Pattern F-F++F-F
-    commands += snowflake(degree - 1);
+    commands += Snowflake(degree - 1);
     commands += "-"; // F-
-    commands += snowflake(degree - 1); 
+    commands += Snowflake(degree - 1);
     commands += "+ + "; // F++
-    commands += snowflake(degree - 1); 
+    commands += Snowflake(degree - 1);
     commands += "-"; // F-
-    commands += snowflake(degree - 1); 
+    commands += Snowflake(degree - 1);
     commands += "-"; // F -
-    commands += snowflake(degree - 1);
+    commands += Snowflake(degree - 1);
 
     return commands;
 }
