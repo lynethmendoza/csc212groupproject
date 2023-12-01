fc17f5b6e79b4d37697b26ad88a3a12db1904cae
JZURI1 <123573769+JZURI1@users.noreply.github.com>
2023-12-01 18:59:16 -0500

    Update kochsnowflake.cpp

--- kochsnowflake.cpp
+++ kochsnowflake.cpp
@@ -13,14 +13,16 @@ int main(){
 
     int degree = 6;
 
+    // Call recursive function
     of << Koch_snowflake(degree);
 }
 // Recursive function for generating the snowflake
 std::string Koch_snowflake(int degree){
+// If the degree is 0 there should be no shape to return    
     if(degree == 0){
         return "";
     }
-// Set axiom (base case) for the snowflake which is F--F--F
+// Set axiom (base case) for the snowflake which is F--F--F or a triangle 
     if(degree == 1){
         return "F--F--F";
     }
@@ -28,7 +30,8 @@ std::string Koch_snowflake(int degree){
 
     std::string newCommands = "";
 
-    for(int i =0; i < commands.size(); i++){
+    // Recursive structure
+    for(int i = 0; i < commands.size(); i++){
         if(commands[i] == 'F'){
             // Koch snowflake recursive pattern F+F--F+F
             newCommands += "F+F--F+F";
@@ -36,6 +39,5 @@ std::string Koch_snowflake(int degree){
             newCommands += commands[i];
         }
     }
-
     return newCommands;
 }
