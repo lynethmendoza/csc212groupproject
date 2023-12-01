4d66a62e928d46075cc9c8c6bcdc7d93d471dcad
JZURI1 <123573769+JZURI1@users.noreply.github.com>
2023-12-01 18:54:47 -0500

    Update kochsnowflake.cpp

--- kochsnowflake.cpp
+++ kochsnowflake.cpp
@@ -20,7 +20,7 @@ std::string Koch_snowflake(int degree){
     if(degree == 0){
         return "";
     }
-
+// Set Axiom for the snowflake which is F--F--F
     if(degree == 1){
         return "F--F--F";
     }
@@ -30,6 +30,7 @@ std::string Koch_snowflake(int degree){
 
     for(int i =0; i < commands.size(); i++){
         if(commands[i] == 'F'){
+            // Snowflake recursive pattern F+F--F+F
             newCommands += "F+F--F+F";
         }else{
             newCommands += commands[i];
