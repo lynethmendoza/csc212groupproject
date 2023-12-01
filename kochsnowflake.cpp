c1b6a34dc16fc4e326ca363f79ab095379f99ab6
lynethmendoza <123670042+lynethmendoza@users.noreply.github.com>
2023-12-01 18:13:28 -0500

    Update kochsnowflake.cpp

--- kochsnowflake.cpp
+++ kochsnowflake.cpp
@@ -5,48 +5,36 @@
 
 // Function declarations
 std::string Koch_snowflake(int degree);
-std::string Snowflake(int degree);
 
 int main(){
 
     // Ofstream to output the restlt of the koch function
     std::ofstream of("l-system.txt");
 
-    int degree = 3;
+    int degree = 6;
 
     of << Koch_snowflake(degree);
 }
-
-// Generates 4 'sides' of the snowflake
-std::string Koch_snowflake(int degree){
-    std::string commands = "";
-
-    for(int i = 0; i < 4; i++){
-        commands += Snowflake(degree);
-        commands += "+ + ";
-    }
-
-    return commands;
-}
-
 // Recursive function for generating the snowflake
-std::string Snowflake(int degree){
+std::string Koch_snowflake(int degree){
     if(degree == 0){
-        return "F ";
+        return "";
     }
-    std::string commands = "";
 
-    // Logic for generating the snowflake
-    // Pattern F-F++F-F
-    commands += Snowflake(degree - 1);
-    commands += "-"; // F-
-    commands += Snowflake(degree - 1);
-    commands += "+ + "; // F++
-    commands += Snowflake(degree - 1);
-    commands += "-"; // F-
-    commands += Snowflake(degree - 1);
-    commands += "-"; // F -
-    commands += Snowflake(degree - 1);
+    if(degree == 1){
+        return "F--F--F";
+    }
+    std::string commands = Koch_snowflake(degree-1);
 
-    return commands;
+    std::string newCommands = "";
+
+    for(int i =0; i < commands.size(); i++){
+        if(commands[i] == 'F'){
+            newCommands += "F+F--F+F";
+        }else{
+            newCommands += commands[i];
+        }
+    }
+
+    return newCommands;
 }
