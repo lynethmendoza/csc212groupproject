38d1b51eec7446538ba409115b3f34e6adf1ba7f
lynethmendoza <123670042+lynethmendoza@users.noreply.github.com>
2023-12-04 13:07:25 -0500

    Update sierpinskitriangle.cpp

--- sierpinskitriangle.cpp
+++ sierpinskitriangle.cpp
@@ -1,7 +1,7 @@
 //Start code used from Lab 6
 //Sierpinski triangle is the idea of drawing smaller, equilateral triangles into one big equilateral triangle, depending on the degree given
 //In this case, degree/order 0 == no triangle
-//This code uses L System commands in order to visually represent the output
+//This code uses L System commands to visually represent the output of the fractal
 
 #include <string>
 #include <iostream>
@@ -41,18 +41,20 @@ std::string sierpinski_triangle(int order){
         //our second base case is that when we reach order 1, we just draw 1 triangle
     }
     
-    std::string commands = sierpinski_triangle(order - 1); //recursive call, this reaches all the way to the base case, then we edit until we reach the order we need
+    std::string commands = sierpinski_triangle(order - 1); //recursive call, this will be called until it reaches the base cases, then we edit until we reach the order we need
 
-    std::string newCommands = ""; //we use this to use the rules in order to create the fractal
+    std::string newCommands = ""; //we use this to use the rules to create the fractal
     //rule 1: whenever there is an F, make it an FF
-    //rule 2: whenever there is an X, make it a --FXF++FXF++FXF--, which makes an upside triangle
+    //rule 2: whenever there is an X, make it a --FXF++FXF++FXF--, which makes an upside-down triangle
 
-    //this loop iterates throught the command string to look at each character to check whether there is an F or X in the current commands
+    //this loop iterates through the command string to look at each character to check whether there is an F or X in the current commands
     for(int i = 0; i < commands.size(); i++){
         if(commands[i] == 'F'){
             newCommands += "FF";
+            //rule 1
         }else if(commands[i] == 'X'){
             newCommands += "--FXF++FXF++FXF--";
+            //rule 2
         }else{
             newCommands += commands[i];
             //if there is neither, you don't change the command, you keep it there
